5517bf1ba5853f5977b044242c713ea1aa7f291d
Ronny B Good <ronny.bruhin+bbc@hotmail.com>
2025-09-12 15:00:45 +0200

    242 | finished | extracts email & pw from form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { Component } from "@angular/core";
+import { FormsModule, NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
+  selector: "app-login",
   standalone: true,
   imports: [FormsModule],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+  templateUrl: "./login.component.html",
+  styleUrl: "./login.component.css",
 })
 export class LoginComponent {
-  onSubmit(form: NgForm) {
-    console.log(form);
-    
+  onSubmit(formData: NgForm) {
+    const enteredEmail = formData.form.value.email;
+    const enteredPassword = formData.form.value.password;
 
+    console.log(enteredEmail, enteredPassword);
   }
 }
