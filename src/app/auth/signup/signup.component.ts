a73c6dfbbbf64e2d2e754b604a84aa7385be57d4
Ronny B Good <ronny.bruhin+bbc@hotmail.com>
2025-09-22 13:10:47 +0200

    255 | Exercise solved for email & pw

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,63 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
+import { of } from "rxjs";
+
+function passwordsDoNotMatch() {
+  return of({passwordsDoNotMatch: false})
+}
 
 @Component({
-  selector: 'app-signup',
+  selector: "app-signup",
   standalone: true,
-  templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css',
+  imports: [ReactiveFormsModule],
+  templateUrl: "./signup.component.html",
+  styleUrl: "./signup.component.css",
 })
-export class SignupComponent {}
+export class SignupComponent {
+  form = new FormGroup({
+    email: new FormControl("", {
+      validators: [Validators.email, Validators.required],
+    }),
+    password: new FormControl("", {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+    confirmPassword: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)]
+    })
+  });
+
+  get emailIsInvalid() {
+    return (
+      this.form.controls.email.touched &&
+      this.form.controls.email.dirty &&
+      this.form.controls.email.invalid
+    );
+  }
+
+  get passwordIsInvalid() {
+    return (
+      this.form.controls.password.touched &&
+      this.form.controls.password.dirty &&
+      this.form.controls.password.invalid
+    );
+  }
+
+  onSubmit() {
+    console.log(this.form);
+    const enteredEmail = this.form.value.email;
+    const enteredPassword = this.form.value.password;
+
+    console.log(enteredEmail);
+    console.log(enteredPassword);
+    
+    this.form.value.email;
+  }
+
+  onReset() {
+  };
+}
