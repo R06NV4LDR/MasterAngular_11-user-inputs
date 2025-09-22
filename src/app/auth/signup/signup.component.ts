de37ce4347006a3d3253e5fac6ab773eab445223
Ronny B Good <ronny.bruhin+bbc@hotmail.com>
2025-09-22 14:19:16 +0200

    259 | added formGroups(pw, address, source)

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import {
+  FormArray,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
@@ -19,24 +20,36 @@ function passwordsDoNotMatch() {
   styleUrl: "./signup.component.css",
 })
 export class SignupComponent {
+  
   form = new FormGroup({
     email: new FormControl("", {
       validators: [Validators.email, Validators.required],
     }),
-    password: new FormControl("", {
-      validators: [Validators.required, Validators.minLength(6)],
-    }),
-    confirmPassword: new FormControl("", {
-      validators: [Validators.required, Validators.minLength(6)],
+    passwords: new FormGroup({
+      password: new FormControl("", {
+        validators: [Validators.required, Validators.minLength(6)],
+      }),
+      confirmPassword: new FormControl("", {
+        validators: [Validators.required, Validators.minLength(6)],
+      }),
     }),
     firstName: new FormControl("", { validators: [Validators.required] }),
     lastName: new FormControl("", { validators: [Validators.required] }),
-    street: new FormControl("", { validators: [Validators.required] }),
-    number: new FormControl("", { validators: [Validators.required] }),
-    postalCode: new FormControl("", { validators: [Validators.required] }),
-    city: new FormControl("", { validators: [Validators.required] }),
-    role: new FormControl<"student" | "teacher" | "employee" | "founder" | "other">("student", { validators:[Validators.required] }),
-    agree: new FormControl(false, { validators: [Validators.required]})
+    address: new FormGroup({
+      street: new FormControl("", { validators: [Validators.required] }),
+      number: new FormControl("", { validators: [Validators.required] }),
+      postalCode: new FormControl("", { validators: [Validators.required] }),
+      city: new FormControl("", { validators: [Validators.required] }),
+    }),
+    role: new FormControl<
+      "student" | "teacher" | "employee" | "founder" | "other"
+    >("student", { validators: [Validators.required] }),
+    source: new FormArray([
+      new FormControl(false),
+      new FormControl(false),
+      new FormControl(false),
+    ]),
+    agree: new FormControl(false, { validators: [Validators.required] }),
   });
 
   get emailIsInvalid() {
@@ -47,21 +60,21 @@ export class SignupComponent {
     );
   }
 
-  get passwordIsInvalid() {
-    return (
-      this.form.controls.password.touched &&
-      this.form.controls.password.dirty &&
-      this.form.controls.password.invalid
-    );
-  }
+  // get passwordIsInvalid() {
+  //   return (
+  //     this.form.controls.password.touched &&
+  //     this.form.controls.password.dirty &&
+  //     this.form.controls.password.invalid
+  //   );
+  // }
 
   onSubmit() {
     console.log(this.form);
     const enteredEmail = this.form.value.email;
-    const enteredPassword = this.form.value.password;
+    // const enteredPassword = this.form.value.password;
 
     console.log(enteredEmail);
-    console.log(enteredPassword);
+    // console.log(enteredPassword);
 
     this.form.value.email;
   }
