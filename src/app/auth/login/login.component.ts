1856d850bfa0cf27dbe782de87826c876b063f7c
Ronny B Good <ronny.bruhin+bbc@hotmail.com>
2025-09-22 10:07:26 +0200

    251 | Validators added

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,31 @@
 import { Component } from "@angular/core";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-login",
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: "./login.component.html",
   styleUrl: "./login.component.css",
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form = new FormGroup({
+    email: new FormControl("", {
+      validators: [Validators.email, Validators.required]
+    }),
+    password: new FormControl("", {
+      validators: [Validators.required, Validators.minLength(6)]
+    }),
+  });
+
+  onSubmit() {
+    console.log(this.form);
+    const enteredEmail = this.form.value.email;
+    const enteredPassword = this.form.value.password;
+    
+    console.log("Entered Email: " + enteredEmail);
+    console.log("Entered PW: " + enteredPassword);
+    
+    this.form.value.email;
+  }
+}
